c50a83cef2a70141b79db25a82bb4e0b1b123b5d
Allegra Witzeneder <witzenedera190233@sus.htl-grieskirchen.at>
2022-09-29 10:03:12 +0200

    Constructor + first Tester

---
+++ Hausuebung2_2909/src/NumberTester.java
@@ -0,0 +1,11 @@
+public class NumberTester {
+
+    private NumberTester oddTester;
+
+    public NumberTester(String fileName) {
+
+    }
+    public void setOddEvenTester(NumberTester oddTester){
+        this.oddTester = oddTester;
+    }
+}
