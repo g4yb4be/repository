6c48757fbfd2c138c09d8dfaf46dc4960ddeb182
Allegra Witzeneder <witzenedera190233@sus.htl-grieskirchen.at>
2022-09-29 09:35:31 +0200

    2 Bsp finished

--- Hausuebung2_2909/src/HalloJavaMitForEach.java
+++ Hausuebung2_2909/src/HalloJavaMitForEach.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class HalloJavaMitForEach {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class HalloJavaMitForEach{
+    private static List<String> liste;
+
+    public static void main(String[] args) {
+     liste = Arrays.asList();
+     liste.forEach(System.out::println);
+    }
 }
+
+
+
