e474f641b58819ddeb06ce6737ea93051c05cfce
Allegra Witzeneder <witzenedera190233@sus.htl-grieskirchen.at>
2022-09-29 10:07:43 +0200

    all Testmethods created

--- Hausuebung2_2909/src/NumberTester.java
+++ Hausuebung2_2909/src/NumberTester.java
@@ -1,11 +1,26 @@
 public class NumberTester {
 
     private NumberTester oddTester;
+    private NumberTester primeTester;
+    private NumberTester palindromeTester;
 
     public NumberTester(String fileName) {
 
     }
+
     public void setOddEvenTester(NumberTester oddTester){
         this.oddTester = oddTester;
+    }
+
+    public void setPrimeTester(NumberTester primeTester){
+        this.primeTester = primeTester;
+    }
+
+    public void setPalindromeTester(NumberTester palindromeTester){
+        this.palindromeTester = palindromeTester;
+    }
+
+    public void testFile(){
+
     }
 }
