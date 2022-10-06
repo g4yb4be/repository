005b29f27101351d9752a670151e31500fd5e38a
Allegra Witzeneder <witzenedera190233@sus.htl-grieskirchen.at>
2022-10-06 10:42:02 +0200

    1.1 done

---
+++ Hausuebung3_0610/src/ReadCSV.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ReadCSV {
+
+    private File csvFile = new File("weapons.csv");
+
+
+    public void readCSV() throws FileNotFoundException {
+        Scanner scanner = new Scanner(csvFile);
+        String delimiter = ";";
+        String line;
+        List<Weapon> lines = new ArrayList();
+        while(scanner.hasNextLine()) {
+            line = scanner.nextLine();
+            String[] arr = line.split(delimiter);
+            Weapon w = new Weapon(arr[0], CombatType.valueOf(arr[1]), DamageType.valueOf(arr[2]), Integer.valueOf(arr[3]), Integer.valueOf(arr[4]),Integer.valueOf(arr[5]),Integer.valueOf(arr[6]));
+            lines.add(w);
+        }
+        lines.forEach(x -> System.out.println(x));
+    }
+}
---
+++ Hausuebung3_0610/src/Weapon.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Weapon {
+    private String name;
+    private CombatType combatType;
+    private DamageType damageType;
+    private int damage;
+    private int speed;
+    private int strength;
+    private int value;
+
+    public Weapon(String name, CombatType combatType, DamageType damageType, int damage, int speed, int strength, int value) throws FileNotFoundException {
+        this.name = name;
+        this.combatType = combatType;
+        this.damageType = damageType;
+        this.damage = damage;
+        this.speed = speed;
+        this.strength = strength;
+        this.value = value;
+    }
+
+
+
+
+
+}
