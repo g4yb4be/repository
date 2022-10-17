1c20a687dc86ad4903b53a220d572dde43effe4a
Allegra Witzeneder <witzenedera190233@sus.htl-grieskirchen.at>
2022-10-17 09:27:12 +0200

    school update

--- Hausuebung4_1310/src/Main.java
+++ Hausuebung4_1310/src/Main.java
@@ -2,6 +2,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -9,12 +11,12 @@ import java.util.concurrent.ThreadPoolExecutor;
 
 public class Main {
     private static ReadCSV readCSV = new ReadCSV();
-    private static ArrayList<Integer> list;
+    private static ArrayList<Integer> intsList;
     private static File csvFile = new File("numbers.csv");
     static Scanner scanner = new Scanner(System.in, "UTF-8");
     static {
         try {
-            list = (ArrayList<Integer>) readCSV.readCSV(csvFile);
+            intsList = (ArrayList<Integer>) readCSV.readCSV(csvFile);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
@@ -27,8 +29,18 @@ public class Main {
         int chunks = Integer.parseInt(scanner.nextLine());
         System.out.println("divider>");
         int divider = Integer.parseInt(scanner.nextLine());
-
         ThreadPoolExecutor executer = (ThreadPoolExecutor) Executors.newFixedThreadPool(chunks);
+        List<Integer> finalList = new ArrayList<>();
+
+        int chunksSize = intsList.size() / chunks;
+        for(int i = 0; i < intsList.size(); i+=chunksSize){
+            if(i + chunksSize < intsList.size()){
+                finalList = intsList.subList(i, i + chunksSize);
+            }else{
+                finalList = intsList.subList(i, intsList.size());
+            }
+        }
+
         executer.execute(integer -> {
             if(integer % divider == 0) System.out.println(integer);
         });
